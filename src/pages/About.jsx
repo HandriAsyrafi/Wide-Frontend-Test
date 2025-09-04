c7113da1c35f18d8f616607015502a2f879220ac
HandriAsyrafi <handriza.asyrafi@gmail.com>
2025-09-04 07:56:12 +0700

    edit About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <>
       <Navbar />
-      <p className="flex justify-around mx-auto w-100 text-xl sm:w-130 md:w-150">
+      <p className="mx-auto border-1 border-gray-300 p-6 shadow-xl rounded-xl w-100 text-xl sm:w-130 md:w-150 ">
         This project is a React.js application built as part of a technical
         test. It demonstrates key React concepts such as state management, form
         handling with validation, API integration, and routing.
